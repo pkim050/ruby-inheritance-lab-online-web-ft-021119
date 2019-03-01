372e7639734d6f263e4f43810b8569da89b24b40
Patrick Kim <pkim050@gmail.com>
2019-03-01 20:23:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,12 @@
 require_relative "./user.rb"
 class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(string)
+    @knowledge << string
+  end
 end
